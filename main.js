d818ce9ddc1276d9f555c59e0fc1d8374745898f
Lorena de Castro <139800465+LorenadeCastro@users.noreply.github.com>
2023-08-07 16:24:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function VerificarImparPar (numero){
+    if (numero % 2 === 0) {
+        console.log(numero + " e um numero par.");
+
+
+    } else {
+        console.log(numero + " nao e um numero par.");
+    }
+    }
+console.log(VerificarImparPar(313));
